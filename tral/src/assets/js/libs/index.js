1a31925f1fee7941a2f03f1cb06f162a09dea572
hzjanger <1305696249@qq.com>
2019-04-12 23:15:50 +0800

    飘城旅行社

--- tral/src/assets/js/libs/index.js
+++ tral/src/assets/js/libs/index.js
@@ -2,12 +2,6 @@ import {loadHeader, loadFooter} from "./loadHtml";
 import $ from 'jquery';
 //加载头部信息
 loadHeader(0);
-console.log('hello world xxx');
 
 //加载脚步信息
 loadFooter();
-
-$(function () {
-    console.log('hello world');
-});
-console.log('hello world');
